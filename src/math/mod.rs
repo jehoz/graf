0a0856d2a3b8fcf6e8eaca7f3e45596488df86d9
jehoz <jesseholzman96@gmail.com>
2026-01-13 15:59:18 -0500

    Add custom Rect type in math

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,3 +1,5 @@
+pub mod rect;
 pub mod v2;
 
+pub use rect::*;
 pub use v2::*;
---
+++ src/math/rect.rs
@@ -0,0 +1,56 @@
+use crate::math::V2;
+
+/// 2D rectangle defined by top-right corner, width, and height
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+
+    pub w: f32,
+    pub h: f32,
+}
+
+impl Rect {
+    pub const fn new(x: f32, y: f32, w: f32, h: f32) -> Self {
+        Self { x, y, w, h }
+    }
+
+    pub fn size(&self) -> V2 {
+        V2::new(self.w, self.h)
+    }
+
+    pub fn top(&self) -> f32 {
+        self.y
+    }
+
+    pub fn left(&self) -> f32 {
+        self.x
+    }
+
+    pub fn bottom(&self) -> f32 {
+        self.y + self.h
+    }
+
+    pub fn right(&self) -> f32 {
+        self.x + self.w
+    }
+
+    pub fn top_left(&self) -> V2 {
+        V2::new(self.x, self.y)
+    }
+
+    pub fn top_right(&self) -> V2 {
+        V2::new(self.right(), self.y)
+    }
+
+    pub fn bottom_left(&self) -> V2 {
+        V2::new(self.x, self.bottom())
+    }
+
+    /// Checks if a given point is inside of the `Rect`
+    pub fn contains(&self, point: V2) -> bool {
+        point.x >= self.left()
+            && point.x < self.right()
+            && point.y >= self.top()
+            && point.y < self.bottom()
+    }
+}
